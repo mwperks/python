ab7ad422ce88a7920eb6b2d353e523c97bfd1f69
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-09-28 20:52:08 +0000

    Else If

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import random
-myName=input("Hello! What is your name?")
-number=random.randint(1, 10)
-print("Well, "+myName+"I am thinking of a number between 1 and 10")
-guess=int(input("Take a guess:"))
-
-if guess==number:
-  print("Good Job,"+myName+"! You guessed my number")
+menuOption=int(input("select an option 1, 2 or 3"))
+if(menuOption==1):
+  print("These are all the Running Trainers")
+elif(menuOption==2):
+  print("These are all the Classics")
+elif(menuOption==3):
+  print("This are all the Boot and Shoes")
 else:
-  print("wrong, better luck next time")
+  print("You didn't choose the correct option")
