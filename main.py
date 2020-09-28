bbeb28aa4864a075f59fbabfd995033606030874
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-09-28 19:43:58 +0000

    new code to create a question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-product_name="iPhone"
-product_price=450
-print(product_name)
-print(product_price)
-print(product_name, product_price)
+name=input("what is your name?")
+print(name)
+print("Hello"+name)
+
+question=input("what city do you live in?")
+
