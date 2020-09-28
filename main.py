c2bf3016205ff49e4aedd9e0eb84294525bdc1cf
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-09-28 21:28:58 +0000

    new code

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-if 2==2:
- print("good job, ")
- print("you are good")
-print("Are we super")
+import random
+myName=input("Hello! What is your name?")
+print(myName)
+age=int(input("can you tell me how old you are?"))
+number=int(input("select a number between 1 and 10"))
+month=int(input("what month are we in"))
+total=age*number*month
+print("total:"+str(total))
+
+
+
