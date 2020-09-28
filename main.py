eda863585809bd53d055d4d2ab0d31a61cc3f8c8
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-09-28 20:43:31 +0000

    practical exercise

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-weather="raining"
-if weather=="raining":
-  print("Take Umbrella")
-else:
-    print("Wear Coat")
-
-weather=input("Is it raining or not?")
-if weather=="raining":
-  print("Take Umbrella")
-else:
-  print("Wear Coat")
+import random
+myName=input("Hello! What is your name?")
+number=random.randint(1, 10)
+print("Well, "+myName+"I am thinking of a number between 1 and 10")
+guess=int(input("Take a guess:"))
+if guess==number:
+  print("Good Job,"+myName+"! You guessed my number")
