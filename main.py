61c3c65c2e91f039014304b1670948d0a493416e
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-09-28 21:06:17 +0000

    What is wrong here exercise

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-menuOption=int(input("select an option 1, 2 or 3"))
-if(menuOption==1):
-  print("These are all the Running Trainers")
-elif(menuOption==2):
-  print("These are all the Classics")
-elif(menuOption==3):
-  print("This are all the Boot and Shoes")
-else:
-  print("You didn't choose the correct option")
+if 2==2:
+ print("good job, ")
+ print("you are good")
+print("Are we super")
