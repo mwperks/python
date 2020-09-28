805f5c5587954bc9e7c57bb11bee97e7604288ac
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-09-28 22:51:26 +0000

    code for Joke

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import random
-num_1=int(input("Enter your first number: "))
-
-num_2=int(input("Enter your second number: "))
-
-choice=str(input("Enter a if you want to add and b if you want to subtract "))
-if choice=="a":
-  print (int(num_1+num_2))
-if choice=="b":
-  print(int(num_1-num_2))
+myName=input("Hello! What is your name? ")
+number=input("What is your favourite number? ")
+if number!=1:
+  print(str("A horse walks into a bar and the bartender says why the long face!"))
