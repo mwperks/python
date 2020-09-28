b2cc20a1be897c5c098083f98da79a7a64b79c8d
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-09-28 20:46:51 +0000

    added selection

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ myName=input("Hello! What is your name?")
 number=random.randint(1, 10)
 print("Well, "+myName+"I am thinking of a number between 1 and 10")
 guess=int(input("Take a guess:"))
+
 if guess==number:
   print("Good Job,"+myName+"! You guessed my number")
+else:
+  print("wrong, better luck next time")
