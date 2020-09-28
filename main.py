fc6c72dcaeb273cc1fc65cff213dbe57bb8c98a4
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-09-28 22:40:01 +0000

    new code for calculator

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-product_name="motorbike"
-price= 2000
-price_reduction =(price *  0.10)
-price = price - price_reduction
-print(str(price)) 
-if (price >1000):
- price_reduction =(price *  0.10)
- price = price - price_reduction
+import random
+num_1=int(input("Enter your first number: "))
+
+num_2=int(input("Enter your second number: "))
+
+choice=str(input("Enter a if you want to add and b if you want to subtract "))
+if choice=="a":
+  print (int(num_1+num_2))
+if choice=="b":
+  print(int(num_1-num_2))
