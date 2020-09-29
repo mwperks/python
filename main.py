67ac95bc1bdd8b68c6fe96ea458c7b8e12505ea5
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-09-29 17:02:43 +0000

    Entering code

--- main.py
+++
@@ -1,8 +0,0 @@
-product_name="motorbike"
-price= 2000
-price_reduction =(price *  0.10)
-price = price - price_reduction
-print(str(price)) 
-if (price >1000):
- price_reduction =(price *  0.10)
- price = price - price_reduction
