2674a3e022077633a8a351d78be6d2347ac10414
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-09-30 16:51:36 +0000

    if and else if

---
+++ main.py
@@ -0,0 +1,8 @@
+def our_if():
+  user_input = input("Enter One ")
+  if user_input=="One":
+    print("Well done - you can read")
+  else:
+    print("Are you sure you should be in here?")
+
+our_if() 
