0cb4a01149cb66fd165451e47d3a95092871715f
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-10-04 21:46:52 +0000

    extended guessing game while loop

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+
 import random
+number_of_goes = 1
 x = random.randrange(1,10)
+print (x)
 guess = int(input("Guess a number between 1 and 10"))
-while guess != x:
-  print ("Sorry, wrong answer, try again. \n")
+while guess != x and number_of_goes < 6:
+  number_of_goes=number_of_goes + 1
+  if (guess < x): 
+    print ("Too low, try again! ")
+  if (guess > x):
+    print ("Too high, try again! ")
+  
   guess = int(input("Guess a number between 1 and 10: "))
-print ("Congratulations!  You guessed correctly!")
+if (guess == x):
+  print ("Congratulations!  You guessed correctly!")
+else:
+  print ("Sorry you have used all your guesses!")
