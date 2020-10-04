fa2c3ed04ec03d1affc4bcfc3dcfc0b6ae09b24d
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-10-04 21:09:09 +0000

    while loop code

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-
-number=int(input("Enter a number between 1 and 10! "))
-if (number==5):
-  print("CORRECT!! Your a genius!")
-else:
-  print("Try again!")
-total = 0
-for i in range(100):
-    for n in range(1,6):
-        generatedNum = 0
-        while generatedNum != n:
-            #generate random number
-            total += 1
-print (total/100)
+number = "5"
+guess = int(input("Guess a number between 1 and 10"))
+while guess != 5:
+  guess = input ("Sorry wrong answer, try again!")
+print ("Congratulations!!  you guessed correctly")
