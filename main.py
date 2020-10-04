0b520bc4868bb1a114040cd9f8aebf4c6334bd97
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-10-04 20:09:44 +0000

    OR

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-age=int(input("What is your age?"))
-if(age>17 and age <60):
-  print("You can learn to drive")
+number=int(input("Enter a number smaller than 10"))
+if(number==1 or number==3 or number==5):
+  print("You entered an ODD number")
 else:
-  print("You cannot learn to drive")
+  print("You entered an even number")
