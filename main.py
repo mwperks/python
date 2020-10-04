67802e023deb6cf323386580d0368a0bc0ff7da7
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-10-04 21:03:04 +0000

    guessing number

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-
-number=int(input("Enter a number between 1 and 10! "))
-if (number==5):
-  print("CORRECT!! Your a genius!")
-else:
-  print("Try again!")
-total = 0
-for i in range(100):
-    for n in range(1,6):
-        generatedNum = 0
-        while generatedNum != n:
-            #generate random number
-            total += 1
-print (total/100)
+import random
+x = random.randrange(1,10)
+guess = int(input("Guess a number between 1 and 10"))
+while guess != x:
+  print ("Sorry, wrong answer, try again. \n")
+  guess = int(input("Guess a number between 1 and 10: "))
+print ("Congratulations!  You guessed correctly!")
