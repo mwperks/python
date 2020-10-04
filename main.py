01d1f71f3d7a3a00da43c30353bb2c1e2fd979d2
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-10-04 22:13:58 +0000

    grade output

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
+def mark_grade(score):
+  if score >= 90: return "A"
+  elif score >= 80: return "B"
+  elif score >= 70: return "C"
+  elif score >= 60: return "D"
+  else : return "E"
 
-import random
-number_of_goes = 1
-x = random.randrange(1,10)
-print (x)
-guess = int(input("Guess a number between 1 and 10"))
-while guess != x and number_of_goes < 6:
-  number_of_goes=number_of_goes + 1
-  if (guess < x): 
-    print ("Too low, try again! ")
-  if (guess > x):
-    print ("Too high, try again! ")
-  
-  guess = int(input("Guess a number between 1 and 10: "))
-if (guess == x):
-  print ("Congratulations!  You guessed correctly!")
-else:
-  print ("Sorry you have used all your guesses!")
+user = int(input ("What is your percentage?"))
+grade = mark_grade(user);
+print (grade)
