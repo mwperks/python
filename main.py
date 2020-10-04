a18205ec2233b59b97aaf204001426eb245a3e34
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-10-04 22:30:45 +0000

    Extra Function code

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ def mark_grade(score):
   else : return "E"
 
 user = int(input ("What is your percentage?"))
+Target = input ("What is your target grade?")
 grade = mark_grade(user);
-print (grade)
+print ("Your target is " + Target + " and your grade is" + grade )
+if grade < Target:
+    print ("You have not passed")
+elif grade == Target:
+    print ("You have achieved your pass mark")
+elif grade > Target:
+    print ("You have passed with honours!")
+else:
+    print ("Need to study more")
