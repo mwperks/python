bee68455bf3949c632d34214568ecb8518e2ce1d
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-10-04 20:06:13 +0000

    logic operator AND

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def menu():
-    menuOption=int(input("Select an option 1,2 or 3"))
-    if(menuOption==1):
-      print("These are all the Running Trainers")
-    elif(menuOption==2):
-      print("these are all the Classics")
-    elif(menuOption==3):
-      print("This is all the Boots and Shoes")
-    else:
-      print("You didn't choose the correct option")
-
-menu()
+age=int(input("What is your age?"))
+if(age>17 and age <60):
+  print("You can learn to drive")
+else:
+  print("You cannot learn to drive")
