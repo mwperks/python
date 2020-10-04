8ba0617a20675b94d4b5f77fab5c144cc6b382c5
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-10-04 21:21:04 +0000

    update code for while number loop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 number = "5"
-guess = int(input("Guess a number between 1 and 10"))
-while guess != 5:
-  guess = input ("Sorry wrong answer, try again!")
+guess = int(input("Guess a number between 1 and 10: "))
+while guess !=5:
+  guess = int(input ("Sorry wrong answer, try again!"))
+
 print ("Congratulations!!  you guessed correctly")
