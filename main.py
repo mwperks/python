b2b69fbd5734c3e5b49e0edd2df90548a8ff3b28
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-10-04 20:44:32 +0000

    guessing number

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-if a = True
-  a = True
-elif b = False
-  b = False
-a=get_user_input_boolean()
+
+number=int(input("Enter a number between 1 and 10! "))
+if (number==5):
+  print("CORRECT!! Your a genius!")
+else:
+  print("Try again!")
+total = 0
+for i in range(100):
+    for n in range(1,6):
+        generatedNum = 0
+        while generatedNum != n:
+            #generate random number
+            total += 1
+print (total/100)
