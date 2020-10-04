8c52abae17d6116cd546c0780aac274551f7fbc7
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-10-04 20:00:57 +0000

    Elif code from lesson

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def our_if():
-  user_input=input("enter one")
-  if user_input == "One":
-      print("Well done - you can read")
-  else:
-    print("Are you sure you should be in here?")
+def menu():
+    menuOption=int(input("Select an option 1,2 or 3"))
+    if(menuOption==1):
+      print("These are all the Running Trainers")
+    elif(menuOption==2):
+      print("these are all the Classics")
+    elif(menuOption==3):
+      print("This is all the Boots and Shoes")
+    else:
+      print("You didn't choose the correct option")
 
-our_if()
+menu()
