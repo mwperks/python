ebf81d348d95a2f1c902f1684e4fc852f9ad110c
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-10-26 11:06:28 +0000

    palindrome exercise

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-palindrome = input("""Please enter a word you think is a palindrome:""")
+palindrome = input("Please enter a word you think is a palindrome:")
 
 palindrome2 = palindrome[::-1]
 
 if palindrome == palindrome2:
   print ("Correct that is a palindrome!")
 else:
-  print("Sorry, try again!")This data will go at the end of a fileThis data will go at the end of a fileThis data will go at the end of a file
+  print("Sorry, try again!")
