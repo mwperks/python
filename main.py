f8b56b797a89d460f56bd0603ea7638eeb38645f
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-10-26 11:04:17 +0000

    code for palindrome

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-myFile=open ("Numbers.txt", "a") 
-myFile.write("This data will go at the end of a file")
+palindrome = input("""Please enter a word you think is a palindrome:""")
 
-myFile.close()
+palindrome2 = palindrome[::-1]
+
+if palindrome == palindrome2:
+  print ("Correct that is a palindrome!")
+else:
+  print("Sorry, try again!")This data will go at the end of a fileThis data will go at the end of a fileThis data will go at the end of a file
