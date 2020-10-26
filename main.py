2455b6e4c596c0fbf0ff6bbebc55169563278d91
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-10-26 10:38:29 +0000

    reading from file code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-myFile=open ("DataFile.txt", "w")
-myFile.write("Hello this is my data")
-myFile.write("I am writing to my file")
+myFile=open ("DataFile.txt", "r") #tells python read only
+fileText=myFile.read() #reads the whole file
+# fileText=myFile.readline() #reads current line only
+print(fileText) #prints out the details
 
 myFile.close()
