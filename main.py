c427a6a061b054ae8616e8b723df355c6adc3483
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-10-26 10:42:34 +0000

    code for creating number file

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-myFile=open ("DataFile.txt", "r") #tells python read only
-fileText=myFile.read() #reads the whole file
-# fileText=myFile.readline() #reads current line only
-print(fileText) #prints out the details
+myFile=open ("Numbers.txt", "w") 
+myFile.write (3, 45, 83, 21)
 
 myFile.close()
