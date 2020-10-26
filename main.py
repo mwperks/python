d2d8a956b3aa55a2d21337ee1bb3286e08d25890
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2020-10-26 12:07:21 +0000

    code for verifying password strength

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-palindrome = input("Please enter a word you think is a palindrome:")
+print("Please enter your password")
+password = input("password ")
+weak = "weak"
+med = "medium"
+strong = "strong"
 
-palindrome2 = palindrome[::-1]
+if len(password) >12:
+  print ("password is too long It must be between 6 to 12 characters")
 
-if palindrome == palindrome2:
-  print ("Correct that is a palindrome!")
+elif len(password) <6:
+  print ("password is too short It must be between 6 and 12 characters")
+
+elif len(password) >=6 and len(password) <= 12:
+  print ("password ok")
+
+if password.lower()== password or password.upper()==password or password.isalnum()==password:
+  print ('password is', weak)
+
+elif password.lower()== password and password.upper()==password or password.isalnum()==password:
+  print ('password is', med)
+
 else:
-  print("Sorry, try again!")
+  password.lower()== password and password.upper()==password and password.isalnum()==password
+  print ('password is', strong)
